6ac00607d3415f3168c09a0db5fabc264494ced2
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2022-01-13 13:52:13 -0500

    Estructura Inicial

---
+++ src/main/java/com/example/gesat/GesatApplication.java
@@ -0,0 +1,13 @@
+package com.example.gesat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GesatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gesat/GesatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gesat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
